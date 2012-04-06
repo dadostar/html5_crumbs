4362955a3b8b8d27774e839c4d5f7e43230ae034
Davide Ceneda <dceneda@yahoo.it>
2012-04-06 11:14:11 +0200

    exercise 8

--- js/script.js
+++ js/script.js
@@ -1,5 +1,135 @@
 !(function (exports) {
 
-  console.log('it works!');
+
+
+
+  var myButtonGreets = $('#mybuttonGreets');
+  var buttonGray = $('#buttonGray');
+  var buttonWhite = $('#buttonWhite');
+  var buttonclear = $('#buttonclear');
+  var containers = $('.container');
+
+  var r = $('#r');
+  var g = $('#g');
+  var b = $('#b');
+
+  var rm = $('#rm');
+  var gm = $('#gm');
+  var bm = $('#bm');
+
+  var rmm = 192;
+  var gmm = 192;
+  var bmm = 192;
+
+var arr = new Array();
+
+  var raggio = $('#raggio');
+  var raggioM = $('#raggioM');
+  var rr = 15;
+
+  var canvas = $('#paper');
+  var ctx = canvas[0].getContext("2d");
+
+
+  var width = canvas.width();
+  var height = canvas.height();
+
+  var drawCircle = function (x, y, radius) {
+    ctx.beginPath();
+    var info = {X:x,Y:y,R:radius};
+    arr.push(info);
+    ctx.arc(x, y, radius, 0, 2*Math.PI, true);
+    ctx.stroke();
+  };
+
+  canvas.on('mousedown',function(e){
+  	drawCircle(e.offsetX,e.offsetY,rr);
+  });
+
+
+  var greets = function (e) {
+    alert('Ciao:)');
+  }
+
+  var drawOldCircles = function(){
+    for(var i=0;i<arr.length;i++)
+    {
+    ctx.beginPath();
+    ctx.arc(arr[i].X, arr[i].Y, arr[i].R, 0, 2*Math.PI, true);
+    ctx.stroke();
+    }
+  }
+
+
+var changeColor = function () {
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = 'rgb('+rmm+','+gmm+','+bmm+')';
+    ctx.fillRect(0, 0, width, height);
+    drawOldCircles();
+    //ctx.fillStyle = 'rgb(0,0,0)';
+    //ctx.lineWidth = 2;
+  };
+  var initCanvas = function () {
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = 'rgb('+rmm+','+gmm+','+bmm+')';
+    ctx.fillRect(0, 0, width, height);
+    arr = new Array();
+    
+    //ctx.fillStyle = 'rgb(0,0,0)';
+    //ctx.lineWidth = 2;
+  };
+
+  buttonclear.on('click',function(e){
+  	initCanvas();
+  });
+
+
+  myButtonGreets.on('click', greets);
+
+  buttonGray.on('click',function(e){
+  	containers.addClass('bg-gray');
+  	containers.removeClass('bg-white');
+  });
+
+
+
+
+buttonWhite.on('click',function(e){
+  	containers.addClass('bg-white');
+  	containers.removeClass('bg-gray');
+  });
+
+
+raggio.on('change', function (e) {
+    var rg = raggio.attr('value');
+
+    rr = rg;
+    raggioM.text(rg);
+  });
+
+r.on('change',function(e){
+  var red = r.attr('value');
+  rmm = red;
+
+  rm.text(red);
+  changeColor();
+});
+
+g.on('change',function(e){
+  var green = g.attr('value');
+  gmm = green;
+
+  gm.text(green);
+  changeColor();
+});
+
+b.on('change',function(e){
+  var blue = b.attr('value');
+  bmm = blue;
+
+  bm.text(blue);
+  changeColor();
+});
+
 
 }(this));
